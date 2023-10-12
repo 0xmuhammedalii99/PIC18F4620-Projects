b155351d0448f4328185a2ab1ba3c77dcc67706a
0xmuhammedalii99 <muhammed9alii9@gmail.com>
2023-10-12 17:37:13 +0300

    Add GPIO Configuration File

---
+++ Fan_Speed_Controller/Code/MCAL_Layer/GPIO_Layer/Hal_GPIO_cfg.h
@@ -0,0 +1,32 @@
+/**
+ * *****************************************************************************************************
+ * * File           : Hal_GPIO_cfg.c
+ * * Auther         : Muhammed Ali Ibrahim Ahmed
+ * * Details        : GPIO
+ * * Linkedin       : https://www.linkedin.com/in/muhammedalii9/
+ * * Date Created   : 29 July,2023
+ * *****************************************************************************************************
+ */
+
+#ifndef HAL_GPIO_CFG_H
+#define	HAL_GPIO_CFG_H
+
+/* _______  Precompile Configration _______ */
+
+#define CONFIG_ENABLE   0x01
+#define CONFIG_DISABLE  0x00
+
+#define GPIO_PIN_CONFIG     CONFIG_ENABLE
+#define GPIO_PORT_CONIG     CONFIG_ENABLE
+
+
+#endif	/* HAL_GPIO_CFG_H */
+
+
+
+/**
+ * ******************************************************************************************************
+ * *      User                   Date                                 Brief
+ * ******************************************************************************************************
+ * *     Muhammed Ali            29 July,2023                         Writting Hal_GPIO_cfg.h
+ */
